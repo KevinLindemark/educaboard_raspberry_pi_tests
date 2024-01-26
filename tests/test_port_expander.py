35e0f35afb1aab5c727762a76bebcbf4f0793b49
kevin <reclaimyourdata@tutamail.com>
2024-01-26 16:03:13 +0100

    comments

--- tests/test_port_expander.py
+++ tests/test_port_expander.py
@@ -6,7 +6,11 @@ spibus.open(0, 1)
 spibus.mode = 0b01
 spibus.max_speed_hz = 5000
 
-# toggle led2 (not sure why this works)
+# LED2 -> GP2
+# LED3 -> GP3
+# ENCCPB -> GP4
+
+# toggle LED2 (not sure why this works)
 spibus.xfer([0x40, 0x0, 0xf0])
 sleep(1)
 spibus.xfer([0x40, 0x0, 0xf])
